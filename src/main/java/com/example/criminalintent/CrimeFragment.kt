e1a53803f2c67a191c8c809474a6a148e22492da
dobrelka <dobrelyatest@gmail.com>
2020-07-17 13:06:05 +0300

    Adding fun onCreateView in CrimeFragment.kt

--- src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -1,6 +1,9 @@
 package com.example.criminalintent
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class CrimeFragment : Fragment() {
@@ -9,5 +12,14 @@ class CrimeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        return view
     }
 }
