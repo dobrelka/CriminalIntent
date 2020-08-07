4a754d4c06979e3369ff441f4aaf7978460c5c30
dobrelka <dobrelyatest@gmail.com>
2020-08-07 17:53:44 +0300

    Replacing holder.apply on holder.bind into CrimeListFragment.kt

--- src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -86,10 +86,8 @@ class CrimeListFragment : Fragment() {
 
          override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
              val crime = crimes[position]
-             holder.apply {
-                 titleTextView.text = crime.title
-                 dateTextView.text = crime.date.toString()
-             }
+
+             holder.bind(crime)
          }
 
 
