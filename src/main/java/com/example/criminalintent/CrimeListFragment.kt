0fc3977a7ce981852f3f9f7975b2b31a47854f64
dobrelka <dobrelyatest@gmail.com>
2020-08-07 12:46:22 +0300

    Adding override fun onCreateView in CrimeListFragment.kt

--- src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -2,9 +2,14 @@ package com.example.criminalintent
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.zip.Inflater
 
 private const val TAG = "CrimeListFragment"
     // CrimeListFragment is an empty shell of a fragment. It logs the number of crimes found in CrimeListViewModel
@@ -19,6 +24,21 @@ class CrimeListFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+    }
+
+    // Setting up the view for CrimeListFragment
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+
+        crimeRecyclerView =
+            view.findViewById(R.id.crime_recycler_view) as RecyclerView
+        crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        return view
     }
 
     companion object {
