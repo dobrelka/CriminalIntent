1f554f2cee5d7beb4431654be2271693bf6582ca
dobrelka <dobrelyatest@gmail.com>
2020-08-13 10:18:28 +0300

    Adding database query functions in CrimeDao.kt

--- src/main/java/com/example/criminalintent/database/CrimeDao.kt
+++ src/main/java/com/example/criminalintent/database/CrimeDao.kt
@@ -1,9 +1,22 @@
 package com.example.criminalintent.database
 
 import androidx.room.Dao
+import androidx.room.Query
+import com.example.criminalintent.Crime
+import java.util.*
 
 // The @Dao annotation lets Room know that CrimeDao
 // is one of your data access objects
 @Dao
 interface CrimeDao {
+
+    // Adding database query functions
+    // Asks Room to pull all columns for all rows in the crime database
+    @Query("SELECT * FROM crime ")
+    fun getCrimes(): List<Crime>
+
+    // Asks Room to pull all columns from only the row whose id matches
+    //the ID value provided
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    fun getCrime(id: UUID): Crime?
 }
