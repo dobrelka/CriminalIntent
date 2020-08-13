aebcbe6f8a9ae146aec176754203af43c640c86f
dobrelka <dobrelyatest@gmail.com>
2020-08-13 15:10:39 +0300

    Adding private val database in CrimeRepository.kt

--- src/main/java/com/example/criminalintent/CrimeRepository.kt
+++ src/main/java/com/example/criminalintent/CrimeRepository.kt
@@ -1,9 +1,23 @@
 package com.example.criminalintent
 
 import android.content.Context
+import androidx.room.Room
+import com.example.criminalintent.database.CrimeDatabase
 import java.lang.IllegalStateException
 
+private const val DATABASE_NAME = "crime-database"
+
 class CrimeRepository private constructor(context: Context) {
+
+    // Room.databaseBuilder() creates a concrete implementation of your abstract CrimeDatabase using
+    //three parameters
+    private val database : CrimeDatabase = Room.databaseBuilder(
+            context.applicationContext,
+            CrimeDatabase::class.java,
+            DATABASE_NAME
+    ).build()
+
+    private val crimeDao = database.crimeDao()
 
     // To make CrimeRepository a singleton, you add two functions to its companion object. One initializes
     //a new instance of the repository, and the other accesses the repository.
