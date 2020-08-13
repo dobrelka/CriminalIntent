1306515a3b1cc5fb55fd143d2edfcddfe886e091
dobrelka <dobrelyatest@gmail.com>
2020-08-13 10:00:46 +0300

    Creating database/CrimeDao.kt

---
+++ src/main/java/com/example/criminalintent/database/CrimeDao.kt
@@ -0,0 +1,9 @@
+package com.example.criminalintent.database
+
+import androidx.room.Dao
+
+// The @Dao annotation lets Room know that CrimeDao
+// is one of your data access objects
+@Dao
+interface CrimeDao {
+}
