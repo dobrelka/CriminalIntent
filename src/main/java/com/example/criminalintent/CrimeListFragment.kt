6bc4a1d1488d1ba542d8fc6fda5779c7bf448ce3
dobrelka <dobrelyatest@gmail.com>
2020-08-14 14:31:44 +0300

    Adding override fun onViewCreated in CrimeListFragment.kt

--- src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -9,6 +9,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -19,7 +20,8 @@ private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
 
     private lateinit var crimeRecyclerView: RecyclerView
-    private var adapter: CrimeAdapter? = null
+    // Initialize the recycler view adapter with an empty crime list to start.
+    private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
@@ -36,9 +38,25 @@ class CrimeListFragment : Fragment() {
         crimeRecyclerView =
             view.findViewById(R.id.crime_recycler_view) as RecyclerView
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
-
+        crimeRecyclerView.adapter = adapter
 
         return view
+    }
+
+    // The LiveData.observe(LifecycleOwner, Observer) function is used to register an observer on
+    //the LiveData instance and tie the life of the observation to the life of another component
+    //The second parameter to the observe(...) function is an Observer implementation,
+    //responsible for reacting to new data from the LiveData
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        crimeListViewModel.crimeListLiveData.observe(
+            viewLifecycleOwner,
+            Observer { crimes ->
+                crimes?.let {
+                    Log.i(TAG, "Got crimes ${crimes.size}")
+                    updateUI(crimes)
+                }
+        } )
     }
 
     private fun updateUI(crimes: List<Crime>) {
