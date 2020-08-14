8deb27ed1c6ea8fd0721eb75fadb028bef6296f9
dobrelka <dobrelyatest@gmail.com>
2020-08-14 12:43:32 +0300

    Replacing val crimes on private val crimeRepository, val crimes in CrimeListViewModel.kt

--- src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -4,14 +4,6 @@ import androidx.lifecycle.ViewModel
 
 class CrimeListViewModel : ViewModel() {
 
-    val crimes = mutableListOf<Crime>()
-
-    init {
-        for (i in 0 until 1000) {
-            val crime = Crime()
-            crime.title = "Crime #$i"
-            crime.isSolved = i % 2 == 0
-            crimes += crime // Generating crimes
-        }
-    }
+    private val crimeRepository = CrimeRepository.get()
+    val crimes = crimeRepository.getCrimes()
 }
