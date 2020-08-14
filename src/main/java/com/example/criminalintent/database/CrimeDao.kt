c26523b896dea38e5794a2866048ee387e80ee88
dobrelka <dobrelyatest@gmail.com>
2020-08-14 13:10:18 +0300

    Replacing interface CrimeDao in CrimeDao.kt

--- src/main/java/com/example/criminalintent/database/CrimeDao.kt
+++ src/main/java/com/example/criminalintent/database/CrimeDao.kt
@@ -1,5 +1,6 @@
 package com.example.criminalintent.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
 import com.example.criminalintent.Crime
@@ -12,11 +13,13 @@ interface CrimeDao {
 
     // Adding database query functions
     // Asks Room to pull all columns for all rows in the crime database
+    // By returning an instance of LiveData from your DAO class, you signal Room to run your query on a
+    //background thread
     @Query("SELECT * FROM crime ")
-    fun getCrimes(): List<Crime>
+    fun getCrimes(): LiveData<List<Crime>>
 
     // Asks Room to pull all columns from only the row whose id matches
     //the ID value provided
     @Query("SELECT * FROM crime WHERE id=(:id)")
-    fun getCrime(id: UUID): Crime?
+    fun getCrime(id: UUID): LiveData<Crime?>
 }
