a8563c51e21e3634bfb692a1393d4d8c8feb4b45
dobrelka <dobrelyatest@gmail.com>
2020-08-18 17:25:00 +0300

    Adding fun updateCrime, fun addCrime in CrimeDao.kt

--- src/main/java/com/example/criminalintent/database/CrimeDao.kt
+++ src/main/java/com/example/criminalintent/database/CrimeDao.kt
@@ -2,7 +2,9 @@ package com.example.criminalintent.database
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.criminalintent.Crime
 import java.util.*
 
@@ -22,4 +24,15 @@ interface CrimeDao {
     //the ID value provided
     @Query("SELECT * FROM crime WHERE id=(:id)")
     fun getCrime(id: UUID): LiveData<Crime?>
+
+    // The updateCrime() function uses the @Update annotation. This function takes in a crime object, uses
+    //the ID stored in that crime to find the associated row, and then updates the data in that row based on the
+    //new data in the crime object
+    @Update
+    fun updateCrime(crime: Crime)
+
+    //The addCrime() function uses the @Insert annotation. The parameter is the crime you want to add to
+    //the database tables
+    @Insert
+    fun addCrime(crime: Crime)
 }
