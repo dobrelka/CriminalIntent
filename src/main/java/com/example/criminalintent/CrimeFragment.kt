f8752a2fb02a12bf8d9c63cef4522a4c2d705d7e
dobrelka <dobrelyatest@gmail.com>
2020-08-18 17:14:59 +0300

    Changed private fun updateUI , adding jumpDrawablesToCurrentState()in CrimeFragment.kt

--- src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -110,7 +110,10 @@ class CrimeFragment : Fragment() {
     private fun updateUI() {
         titleField.setText(crime.title)
         dateButton.text = crime.date.toString()
-        solvedCheckBox.isChecked = crime.isSolved
+        solvedCheckBox.apply {
+            isChecked = crime.isSolved
+            jumpDrawablesToCurrentState()
+        }
     }
 
     // When the hosting activity needs an instance of that fragment,
