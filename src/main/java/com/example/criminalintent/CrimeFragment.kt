d9d2f64ec74e3837dcda7879dda50454db1774b8
dobrelka <dobrelyatest@gmail.com>
2020-08-19 17:14:45 +0300

    Adding DatePickerFragment.Callbacks, override fun onDateSelected in CrimeFragment.kt

--- src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -21,7 +21,7 @@ private const val ARG_CRIME_ID = "crime_id"
 private const val DIALOG_DATE = "DialogDate"
 private const val REQUEST_DATE = 0
 
-class CrimeFragment : Fragment() {
+class CrimeFragment : Fragment(), DatePickerFragment.Callbacks {
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
     private lateinit var dateButton: Button
@@ -116,6 +116,11 @@ class CrimeFragment : Fragment() {
     override fun onStop() {
         super.onStop()
         crimeDetailViewModel.saveCrime(crime)
+    }
+
+    override fun onDateSelected(date: Date) {
+        crime.date = date
+        updateUI()
     }
 
     private fun updateUI() {
