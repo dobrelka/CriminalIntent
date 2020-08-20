d02e9852eb10062709d7f43515895f38aca23316
dobrelka <dobrelyatest@gmail.com>
2020-08-20 12:01:41 +0300

    Adding override fun onCreateOptionsMenu in CrimeListFragment.kt

--- src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -3,9 +3,7 @@ package com.example.criminalintent
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -79,6 +77,13 @@ class CrimeListFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         callbacks = null
+    }
+
+    // Within this function, you call MenuInflater.inflate(Int, Menu) and pass in the resource ID of your
+    //menu file
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_crime_list, menu)
     }
 
     private fun updateUI(crimes: List<Crime>) {
