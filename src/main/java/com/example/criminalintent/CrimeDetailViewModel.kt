2354d8da02a1cd101b684867bd1463f0d32b8144
dobrelka <dobrelyatest@gmail.com>
2020-08-26 16:46:54 +0300

    Adding fun getPhotoFile in CrimeDetailViewModel.kt

--- src/main/java/com/example/criminalintent/CrimeDetailViewModel.kt
+++ src/main/java/com/example/criminalintent/CrimeDetailViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import java.io.File
 import java.util.*
 
 class CrimeDetailViewModel() : ViewModel() {
@@ -25,5 +26,9 @@ class CrimeDetailViewModel() : ViewModel() {
     //SaveCrime(Crime) accepts a Crime and writes it to the database
     fun saveCrime(crime: Crime) {
         crimeRepository.updateCrime(crime)
+    }
+
+    fun getPhotoFile(crime: Crime): File {
+        return  crimeRepository.getPhotoFile(crime)
     }
 }
