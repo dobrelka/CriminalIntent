f833bb8f25c4dc7ad0b4aae8c7a5513ed6e6c7e3
dobrelka <dobrelyatest@gmail.com>
2020-08-27 15:18:08 +0300

    Adding fun getScaledBitmap in PictureUtils.kt

--- src/main/java/com/example/criminalintent/PictureUtils.kt
+++ src/main/java/com/example/criminalintent/PictureUtils.kt
@@ -1,7 +1,17 @@
 package com.example.criminalintent
 
+import android.app.Activity
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Point
+
+// This function checks to see how big the screen is and then scales the image down to that size.
+fun getScaledBitmap(path: String, activity: Activity): Bitmap {
+    val size = Point()
+    activity.windowManager.defaultDisplay.getSize(size)
+
+    return getScaledBitmap(path, size.x, size.y)
+}
 
 fun getScaledBitmap(path: String, destWidth: Int, destHeight: Int): Bitmap {
     //Read in the dimensions of the image on disk
